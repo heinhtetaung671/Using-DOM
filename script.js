2b5dcb0bbf2adaa62798737f1d71e3ec6ceee1ec
heinhtetaung671 <hhein8358@gmail.com>
2024-02-29 17:05:43 +0630

    learn about dom security and accessing attribute

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ let elInfoArea = document.getElementById('infoArea');
 
 elButton.addEventListener('click', () => showMoreAboutMe())
 
+document.addEventListener('DOMContentLoaded', () => {
+   let parent = document.getElementsByClassName('parent')[0]
+   parent.firstChild.nodeValue = 'Hey! '
+   parent.lastChild.nodeValue = ' Hein Htet Aung'
+
+   let displayArea = document.getElementsByClassName('displayArea')[0]
+   let msg = '<p> Text Content : ' + parent.textContent + '</p>';
+   msg += '<p> Inner Text : ' + parent.innerText + '</p>';
+   displayArea.innerHTML = msg;
+})
+
 function showMoreAboutMe( ){
    let me = {
       name: 'Hein Htet Aung',
